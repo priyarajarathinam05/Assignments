a7b996a43bba687c4f02c97ecf0024871270a284
priyarajarathinam05 <priya.rajarathinam@gmail.com>
2026-05-15 20:21:18 +0530

    Add files via upload
    week1 -3 and 4 th assignments completed

---
+++ gradeCalculation.js
@@ -0,0 +1,25 @@
+
+function gradeCalculation(marks){ 
+
+       
+    switch (true) {
+        case (marks>=35 && marks < 50):        
+            console.log("Grade is D")
+        break;
+           case (marks>=50 && marks < 75):        
+            console.log("Grade is C")
+        break;
+
+        case   (marks>=75 && marks < 90):        
+            console.log("Grade is B")
+        break;
+        case   (marks>=90 && marks <=100):        
+            console.log("Grade is A")
+        break;
+       default:
+            console.log("Better Luck next time")
+                      break;
+    }
+
+}
+gradeCalculation(60)
