0126fe0ffd461fd20a76305c4719a72efa7b0e96
priyarajarathinam05 <priya.rajarathinam@gmail.com>
2026-05-20 13:53:19 +0530

    Add files via upload

--- stringAssignmentEx1.js
+++ stringAssignmentEx1.js
@@ -1,6 +1,14 @@
 //code to return the length of last word
-let word="Hello World"
-let valueAfter=word.split(" ")
-let lenAftersplit=valueAfter.length
-let lenOfLastWord=valueAfter[lenAftersplit-1].length
-console.log(lenOfLastWord)
+function lengthOf(word){
+
+    let afterSplit = word.split(" ")//returns the value after split applied
+
+    let lenOfSplit = afterSplit.length;
+
+    let lastWordlen = afterSplit[lenOfSplit-1].length//gives the length of the lastword
+ 
+    return lastWordlen;
+
+}
+result = lengthOf("Hello World")
+console.log(result)
