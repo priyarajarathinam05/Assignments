a831fe5c0abcb3142062e8a3c4abe4af4784ef90
priyarajarathinam05 <priya.rajarathinam@gmail.com>
2026-05-20 13:55:58 +0530

    Add files via upload
    rectified the algorithm

--- stringAssignmentEx3.js
+++ stringAssignmentEx3.js
@@ -1,46 +1,33 @@
 //code to find a word anagaram or not 
 
-function isAnagram(a,b){
-      a=a.toLowerCase()
-      a=a.split(" ").join("")
-      console.log(a)
-         b=b.toLowerCase()
-      b=b.split(" ").join("")
-        let finalcount=0
-    if(a.length==b.length)
-    {
-        for(let i=0;i<a.length;i++)
-        {
-             let count=0
-            for(let j=0;j<a.length;j++)
-            {
-                if(a[i]==b[j])              
-                {
-                    count++
-                                      
-               }
-            }
-            if(count==0){
-                finalcount=0               
-                 console.log("The letter  "+ a[i]  +"  is not present")
-            }
-            else if (count>1) {             
-                 let countdupli=1                
-               finalcount=finalcount+countdupli               
-            } 
-            else{            
-               finalcount++
-            }          
-        }
-                    
-    }
-console.log(finalcount)  
-if(finalcount==a.length){
-    console.log("the word is anagram")
-}
-else{
- console.log("the word is not anagram")
-}
-}
-res=isAnagram("the eyes","they see")
+function isAnagram(string1,string2){
+     const str1=string1.toLowerCase().split("").join("") //converts the char into lower case and remove any spaces in between
+      
+     const str2=string2.toLowerCase().split("").join("")
 
+    if(str1.length === str2.length)//comparing the length and sorting
+         {
+          const  strsorted1=str1.split("").sort().join("");
+           
+           const  strsorted2=str2.split("").sort().join("");
+
+          const result= strsorted1 === strsorted2//comparing the sorted strings 
+
+          if(result === true)
+               {
+            console.log(result  +"     the words given are   anagram")
+          }
+          else
+            {      
+    
+             console.log(result  +"     the words given are not  anagram")
+          }
+        return result
+        }                      
+
+    else
+        {
+        console.log("the strings are not Anagram")
+    }
+}
+ result = isAnagram("the eyes","they see")
